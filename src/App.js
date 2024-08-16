3da2e25d78b29b1374fc32f9a3ef3899049f8b60
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2024-08-16 14:51:55 +0330

    adding last section

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ export default function Website() {
         <NavBar />
         <Header />
         <SectionContainer />
-        <Boody />
       </Background>
     </>
   );
--- src/SectionContainer.js
+++ src/SectionContainer.js
@@ -4,6 +4,10 @@ import video3 from "./page_content/videos/video3.webm";
 import video4 from "./page_content/videos/video4.webm";
 import video5 from "./page_content/videos/video5.webm";
 import video6 from "./page_content/videos/video6.webm";
+import wumpus from "./page_content/pictures/662fa569e650b7bdf2f5d8b4_WUMPUS_LEAN_MOUTH.webp";
+import wumpusHat from "./page_content/pictures/664f28347027689069a4147e_wumpus.webp";
+import noMoreScrollImg from "./page_content/pictures/6658cc069d1eb1caf9426914_Footer-Art_cut.webp";
+import { DownloadSvg } from "./returnSvg";
 
 const sectionsData = [
   {
@@ -47,17 +51,17 @@ const sectionsData = [
 function Section({ data }) {
   return (
     <section
-      className={`w-[95%] rounded-[40px] min-[1250px]:rounded-[60px] min-[1250px]:max-w-[1300px] flex flex-col ${
+      className={`w-[85%] rounded-[40px] min-[1250px]:rounded-[60px] min-[1250px]:max-w-[1300px] flex flex-col ${
         data.videoLocation === "right"
           ? "min-[760px]:flex-row"
           : "min-[760px]:flex-row-reverse"
       } gap-[40px] items-center justify-center min-[1250px]:justify-between bg-gradient-to-tl from-[#fff3] to-[#ffffff12] p-[15px] min-[760px]:py-[60px] min-[760px]:px-[25px] min-[1250px]:p-[15px] shadow-[0_2px_52px_#452a7c1a,inset_0_0_0_3px_#fff3]`}
     >
-      <div className="min-[760px]:w-[40%] min-[1250px]:w-[35%] min-[1250px]:mx-[45px]">
-        <h2 className="font-abcgintonord text-white text-[27px] uppercase w-[80%] min-[760px]:w-full mt-[50px] min-[760px]:mt-0 min-[1250px]:text-[2.2rem] leading-[1.2]">
+      <div className="min-[760px]:w-[40%] min-[1250px]:w-[35%] mx-[45px] min-[1250px]:mx-[45px]">
+        <h2 className="font-abcgintonord text-white text-[27px] uppercase mt-[50px] min-[760px]:mt-0 min-[1250px]:text-[2.2rem] leading-[1.2]">
           {data.header}
         </h2>
-        <p className="w-[80%] min-[760px]:w-full min-[1250px]:w-[95%] min-[1250px]:text-[1.5rem] font-Ggsans text-white mt-[30px] min-[760px]:mt-[24px] min-[1250px]:mt-[15px] mb-[20px] min-[760px]:mb-0">
+        <p className="min-[1250px]:w-[95%] min-[1250px]:text-[1.5rem] font-Ggsans text-white mt-[30px] min-[760px]:mt-[24px] min-[1250px]:mt-[15px] mb-[20px] min-[760px]:mb-0">
           {data.prag}
         </p>
       </div>
@@ -74,6 +78,35 @@ function Section({ data }) {
   );
 }
 
+function NoScrollSection() {
+  return (
+    <section className="flex flex-col gap-[30px] min-[990px]:gap-[60px] justify-center items-center bg-star max-w-[1280px]">
+      <h2 className="text-white font-abcgintonord uppercase text-[2rem] min-[990px]:text-[2.25rem] min-[1280px]:text-[3rem] text-center leading-[1.2]">
+        you can't scroll anymore.
+        <br />
+        better go chat
+      </h2>
+      <button className="font-Ggsans-medium flex gap-3 text-white text-[20px] bg-[#5865F2] py-3 px-8 rounded-full transition-all hover:bg-[#8891f2] hover:translate-y-[-2px]">
+        <DownloadSvg color={"#ffffff"} />
+        Download for Windows
+      </button>
+      <div className="relative">
+        <img src={noMoreScrollImg} alt="no more scroll" />
+        <img
+          src={wumpus}
+          alt="wumpus lean mouth"
+          className="absolute w-[12%] left-[41%] bottom-0"
+        />
+        <img
+          src={wumpusHat}
+          alt="wumpus lean mouth hat"
+          className="absolute top-[20%] w-[6%] left-[44%]"
+        />
+      </div>
+    </section>
+  );
+}
+
 export default function SectionContainer() {
   return (
     <div className="flex flex-col justify-between items-center gap-[225px] mt-[130px] min-[1250px]:mt-[200px]">
@@ -83,6 +116,7 @@ export default function SectionContainer() {
       <Section data={sectionsData[3]} />
       <Section data={sectionsData[4]} />
       <Section data={sectionsData[5]} />
+      <NoScrollSection />
     </div>
   );
 }
